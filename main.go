414b3c74b61af33fe7ec632e60154245cfb25174
Adele Noronha <adele.c.noronha@gmail.com>
2023-07-31 16:54:13 -0700

    create makePizza function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -30,12 +31,62 @@ func (p *Producer) Close() error {
 	return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	// increment pizza number by 1
+	pizzaNumber++
+
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Recieved order number #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		successs := false
+
+		// Select State conditionals
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza number %d. It will take %d seconds.\n", pizzaNumber, delay)
+
+		// delay a bit
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingrediants for pizza #%d!", rune(pizzaNumber))
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The chef quit before making for pizza #%d!", rune(pizzaNumber))
+		} else {
+			successs = true
+			msg = fmt.Sprintf("Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			successs:    successs,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
 func pizzeria(pizzaMaker *Producer) {
 	// keep track of which pizza we are making
+	var i = 0
 
 	// run forever or until we recieve quit notification
 	for {
 		// when running, try to make pizzas (in background)
+		currentPizza := makePizza(i)
 
 		// Select State (conditionals)
 	}
